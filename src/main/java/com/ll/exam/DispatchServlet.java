23afe2190c0dfe6b3bacfedc49fe0a36781c9191
이주원 <won@ijuwon-ui-MacBookAir.local>
2022-08-08 21:53:57 +0900

    setting

---
+++ src/main/java/com/ll/exam/DispatchServlet.java
@@ -0,0 +1,19 @@
+package com.ll.exam;
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/usr/*")
+public class DispatchServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().append("Work?");
+    }
+
+}
