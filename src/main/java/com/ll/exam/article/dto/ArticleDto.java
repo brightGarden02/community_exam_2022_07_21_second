95860b384b13aacb05e6311740690b71917b56db
brightGarden02 <rreone2017@gmail.com>
2022-08-10 01:06:31 +0900

    feat: ArticleDto에 @Data 어노테이션 적용

--- src/main/java/com/ll/exam/article/dto/ArticleDto.java
+++ src/main/java/com/ll/exam/article/dto/ArticleDto.java
@@ -1,21 +1,18 @@
 package com.ll.exam.article.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
-@ToString
+@Data
 @AllArgsConstructor
 public class ArticleDto {
 
-    @Getter @Setter
+
     private long id;
 
-    @Getter @Setter
+
     private String title;
 
-    @Getter @Setter
+
     private String body;
 
 }
