2895407c7f61b2f12f7464368ff762066bc59692
brightGarden02 <rreone2017@gmail.com>
2022-08-10 01:05:07 +0900

    롬북 적용, ArticleDto에 롬복 적용

--- src/main/java/com/ll/exam/article/dto/ArticleDto.java
+++ src/main/java/com/ll/exam/article/dto/ArticleDto.java
@@ -1,47 +1,21 @@
 package com.ll.exam.article.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@AllArgsConstructor
 public class ArticleDto {
 
+    @Getter @Setter
     private long id;
+
+    @Getter @Setter
     private String title;
+
+    @Getter @Setter
     private String body;
 
-    public ArticleDto(long id, String title, String body) {
-        this.id = id;
-        this.title = title;
-        this.body = body;
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-
-    @Override
-    public String toString() {
-        return "ArticleDto{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", body='" + body + '\'' +
-                '}';
-    }
 }
